e3f912bf5a1d6f846a991d2aa66450693a4fefc3
James <jammyw@gmail.com>
2019-12-01 10:31:54 +0000

    day01

---
+++ src/days/day01.rs
@@ -0,0 +1,37 @@
+use crate::error::Error;
+
+pub fn part1(input: &str) -> Result<(),Error> {
+    let masses = parse_masses(input)?;
+    let sum_fuel_reqs: i64 = masses.into_iter().map(fuel_req).sum();
+    println!("Sum of fuel required: {}", sum_fuel_reqs);
+    Ok(())
+}
+
+pub fn part2(input: &str) -> Result<(),Error> {
+    let masses = parse_masses(input)?;
+    let mut sum_fuel_reqs: i64 = masses.into_iter().map(recursive_fuel_req).sum();
+    println!("Recursive sum of fuel required: {}", sum_fuel_reqs);
+    Ok(())
+}
+
+fn parse_masses(input: &str) -> Result<Vec<i64>,Error> {
+    let mut masses = Vec::new();
+    for (idx, line) in input.lines().enumerate() {
+        let n = line
+            .parse()
+            .map_err(|e| format!("Error on line {} of input: {}", idx+1, e))?;
+        masses.push(n);
+    }
+    Ok(masses)
+}
+
+fn fuel_req(mass: i64) -> i64 {
+    let fuel = mass / 3 - 2;
+    if fuel < 0 { 0 } else { fuel }
+}
+
+fn recursive_fuel_req(mass: i64) -> i64 {
+    let extra_mass = fuel_req(mass);
+    extra_mass + if extra_mass > 0 { recursive_fuel_req(extra_mass) } else { 0 }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+#[macro_use] mod error;
+mod days;
+
+use error::Error;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "aoc2019", about = "AoC2019 solutions")]
+enum Day {
+    Day1 {
+        #[structopt(name = "FILE", parse(from_os_str))]
+        input: PathBuf,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = day(Day::from_args()) {
+        eprintln!("{}", e);
+    }
+}
+
+fn day(day: Day) -> Result<(),Error> {
+    use self::Day::*;
+    match day {
+        Day1 { input } => {
+            let s = read(input)?;
+            days::day01::part1(&s)?;
+            days::day01::part2(&s)?;
+        }
+    };
+    Ok(())
+}
+
+fn read(path: PathBuf) -> Result<String,Error> {
+    use std::io::Read;
+    let mut file = std::fs::File::open(path)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
 }
