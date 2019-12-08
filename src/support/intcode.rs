c9e0ab2cc7797f5e9f392846c0536dd694698f06
James Wilson <james@jsdw.me>
2019-12-08 23:42:22 +0000

    wee tidy in intcode

--- src/support/intcode.rs
+++ src/support/intcode.rs
@@ -28,31 +28,31 @@ impl Intcode {
     }
     pub fn step(&mut self) -> Result<Option<Outcome>,Error> {
         let pos = self.position;
-        let instr = if let Some(&pos) = self.ops.get(pos) {
-            Instruction::new(pos as usize)
+        let instr = if let Some(&op_value) = self.ops.get(pos) {
+            Instruction::new(op_value as usize)
         } else {
             return Err(err!("Out of bound access of position {}", pos));
         };
 
         match instr {
             Instruction::Add(c,b,a) => {
-                let c = self.try_get_value(c, pos+1)?;
-                let b = self.try_get_value(b, pos+2)?;
-                let a = self.try_get_pos(a, pos+3)?;
+                let c = self.try_get_value(c,1)?;
+                let b = self.try_get_value(b,2)?;
+                let a = self.try_get_pos(a,3)?;
                 self.ops[a] = b + c;
                 self.position += 4;
                 Ok(Some(Outcome::StepComplete))
             },
             Instruction::Mul(c,b,a) => {
-                let c = self.try_get_value(c, pos+1)?;
-                let b = self.try_get_value(b, pos+2)?;
-                let a = self.try_get_pos(a, pos+3)?;
+                let c = self.try_get_value(c,1)?;
+                let b = self.try_get_value(b,2)?;
+                let a = self.try_get_pos(a,3)?;
                 self.ops[a] = b * c;
                 self.position += 4;
                 Ok(Some(Outcome::StepComplete))
             },
             Instruction::Input(c) => {
-                let c = self.try_get_pos(c, pos+1)?;
+                let c = self.try_get_pos(c,1)?;
                 // Computation is essentially suspended until
                 // this input provider is given input. If it's dropped
                 // without being given input, we'll be given another
@@ -63,14 +63,14 @@ impl Intcode {
                 })))
             },
             Instruction::Output(c) => {
+                let c = self.try_get_value(c,1)?;
                 self.position += 2;
-                let c = self.try_get_value(c, pos+1)?;
                 Ok(Some(Outcome::Output(c)))
             },
             Instruction::JumpIfTrue(c,b) => {
-                let c = self.try_get_value(c, pos+1)?;
+                let c = self.try_get_value(c,1)?;
                 if c != 0 {
-                    let b = self.try_get_value(b, pos+2)?;
+                    let b = self.try_get_value(b,2)?;
                     self.position = b as usize;
                 } else {
                     self.position += 3;
@@ -78,9 +78,9 @@ impl Intcode {
                 Ok(Some(Outcome::StepComplete))
             },
             Instruction::JumpIfFalse(c,b) => {
-                let c = self.try_get_value(c, pos+1)?;
+                let c = self.try_get_value(c,1)?;
                 if c == 0 {
-                    let b = self.try_get_value(b, pos+2)?;
+                    let b = self.try_get_value(b,2)?;
                     self.position = b as usize;
                 } else {
                     self.position += 3;
@@ -88,17 +88,17 @@ impl Intcode {
                 Ok(Some(Outcome::StepComplete))
             },
             Instruction::LessThan(c,b,a) => {
-                let c = self.try_get_value(c, pos+1)?;
-                let b = self.try_get_value(b, pos+2)?;
-                let a = self.try_get_pos(a, pos+3)?;
+                let c = self.try_get_value(c,1)?;
+                let b = self.try_get_value(b,2)?;
+                let a = self.try_get_pos(a,3)?;
                 self.ops[a] = if c < b { 1 } else { 0 };
                 self.position += 4;
                 Ok(Some(Outcome::StepComplete))
             },
             Instruction::Equals(c,b,a) => {
-                let c = self.try_get_value(c, pos+1)?;
-                let b = self.try_get_value(b, pos+2)?;
-                let a = self.try_get_pos(a, pos+3)?;
+                let c = self.try_get_value(c,1)?;
+                let b = self.try_get_value(b,2)?;
+                let a = self.try_get_pos(a,3)?;
                 self.ops[a] = if c == b { 1 } else { 0 };
                 self.position += 4;
                 Ok(Some(Outcome::StepComplete))
@@ -109,11 +109,13 @@ impl Intcode {
         }
 
     }
-    fn try_get_pos(&self, ty: VarType, pos: usize) -> Result<usize,Error> {
+    fn try_get_pos(&self, ty: VarType, offset: usize) -> Result<usize,Error> {
+        let pos = self.position + offset;
         ty.get_pos(&self.ops, pos)
             .ok_or_else(|| err!("can't get value at index {} in ops", pos))
     }
-    fn try_get_value(&self, ty: VarType, pos: usize) -> Result<i64,Error> {
+    fn try_get_value(&self, ty: VarType, offset: usize) -> Result<i64,Error> {
+        let pos = self.position + offset;
         ty.get_value(&self.ops, pos)
             .ok_or_else(|| err!("can't get value at index {} in ops", pos))
     }
