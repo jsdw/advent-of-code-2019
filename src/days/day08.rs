65e96ae72257073688e6b7607fae13684cb4df0d
James Wilson <james@jsdw.me>
2019-12-16 20:04:38 +0000

    golf day8 a bit

--- src/days/day08.rs
+++ src/days/day08.rs
@@ -1,55 +1,31 @@
 use crate::error::Error;
 
 pub fn part1(input: &str, width: usize, height: usize) -> Result<(), Error> {
-    let layers = parse_data(input, width, height)?;
-
     // Find layer with least 0's:
-    let mut layer_with_least_zeros = 0;
-    let mut zeros = std::usize::MAX;
-    for (idx, layer) in layers.iter().enumerate() {
-        let zs = count_digit(layer, 0);
-        if zs < zeros {
-            layer_with_least_zeros = idx;
-            zeros = zs;
-        }
-    }
+    let ls = parse_data(input, width, height);
+    let l = ls
+        .iter()
+        .min_by_key(|l| count_digit(l,0))
+        .unwrap();
 
-    // Multiply the 1's and 2's of this layer
-    let layer = &layers[layer_with_least_zeros];
-    let ones = count_digit(layer, 1);
-    let twos = count_digit(layer, 2);
-    println!("Star 1: {}", ones * twos);
-
+    println!("Star 1: {}", count_digit(l, 1) * count_digit(l, 2));
     Ok(())
 }
 
 pub fn part2(input: &str, width: usize, height: usize) -> Result<(), Error> {
-    let layers = parse_data(input, width, height)?;
+    let layers = parse_data(input, width, height);
     let size = width * height;
 
     // Merge layers (0 black, 1 white, 2 transparent):
-    let mut output_layer = vec![2; size];
-    for i in 0..size {
-        let mut n = output_layer[i];
-        for layer in &layers {
-            if n == 2 {
-                n = layer[i];
-            }
-        }
-        output_layer[i] = n;
-    }
+    let l: Vec<u8> = (0..size)
+        .map(|i| layers.iter().map(|l| l[i]).filter(|&n| n != 2).next().unwrap())
+        .collect();
 
     // Print output to console:
     println!("Star 2:");
-    for row in output_layer.chunks(width) {
-        let s: String = row.iter().map(|n| {
-            match n {
-                0 => '#',
-                1 => '.',
-                _ => ' '
-            }
-        }).collect();
-        println!("{}", s);
+    for row in l.chunks_exact(width) {
+        row.iter().for_each(|&n| print!("{}", if n == 1 { '■' } else { ' '}));
+        println!();
     }
 
     Ok(())
@@ -59,23 +35,10 @@ fn count_digit(layer: &[u8], digit: u8) -> usize {
     layer.iter().filter(|&&n| n == digit).count()
 }
 
-fn parse_data(input: &str, width: usize, height: usize) -> Result<Vec<Vec<u8>>,Error> {
-    let input = input.trim();
-    let size = width * height;
-    if input.len() % size != 0 {
-        return Err(err!("Size of data is not an exact multiple of layer size"));
-    }
-
-    let mut layers = Vec::with_capacity(input.len() / size);
-    let mut layer = Vec::with_capacity(size);
-    for (idx,c) in input.chars().enumerate() {
-        let n = c.to_digit(10).ok_or_else(|| err!("Character {} in input is not a number", idx+1))?;
-        layer.push(n as u8);
-        if layer.len() == size {
-            layers.push(layer);
-            layer = Vec::with_capacity(size);
-        }
-    }
-
-    Ok(layers)
+fn parse_data(input: &str, width: usize, height: usize) -> Vec<Vec<u8>> {
+    input.trim()
+         .as_bytes()
+         .chunks_exact(width*height)
+         .map(|c| c.into_iter().map(|b| b-48).collect())
+         .collect()
 }
