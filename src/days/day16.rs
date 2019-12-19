4ba39d5171904112460af7c706541754ab7937f9
James Wilson <james@jsdw.me>
2019-12-19 00:22:28 +0000

    Optimise day16 (part2 not done yet)

--- src/days/day16.rs
+++ src/days/day16.rs
@@ -1,51 +1,55 @@
 use crate::error::Error;
 use std::iter;
+use rayon::prelude::*;
 
 pub fn both_parts(input: &str) -> Result<(), Error> {
     let vals = parse_input(input);
 
     println!("Star 1: {}", stringify(&phases(vals.clone(), 100), 0, 8));
 
-    println!("Star 2: {}", stringify(&phases(vals[0..10].repeat(10), 100), 0, 8));
-
-    // let more_vals = vals.repeat(10_000);
-    // let skip: usize = stringify(&more_vals, 0, 7).parse().unwrap();
-    // println!("Star 2: {}", stringify(&phases(more_vals, 100), skip, 8));
+    let more_vals = vals.repeat(10_000);
+    let skip: usize = stringify(&more_vals, 0, 7).parse().unwrap();
+    println!("Star 2: {}", stringify(&phases(more_vals, 100), skip, 8));
 
     Ok(())
 }
 
-fn stringify(input: &[i32], offset: usize, limit: usize) -> String {
+fn stringify(input: &[i8], offset: usize, limit: usize) -> String {
     input.iter().skip(offset).take(limit).map(|n| n.to_string()).collect()
 }
 
-fn phases(mut input: Vec<i32>, n: usize) -> Vec<i32> {
-    for _ in 0..n {
-        // println!("{:?}", input[0..50].iter().map(|&n| if n == 0 { '#' } else { ' ' }).collect::<Vec<_>>() );
+fn phases(mut input: Vec<i8>, n: usize) -> Vec<i8> {
+    for i in 0..n {
+        println!("Phase {}", i);
         input = phase(&input);
     }
     input
 }
 
-fn phase(input: &[i32]) -> Vec<i32> {
-    (0..input.len()).map(|n| {
-        let n: i32 = input.into_iter()
-            .zip(repeating_pattern(n))
-            .map(|(a,b)| a*b)
-            .sum();
-        (n % 10).abs()
+fn phase(input: &[i8]) -> Vec<i8> {
+    (0..input.len()).into_par_iter().map(|n| {
+        let out: i32 = repeating_indexes(n, input).map(|(arr,r)| {
+            arr.into_iter().map(|&n| n as i32).sum::<i32>() * r
+        }).sum();
+        (out % 10).abs() as i8
     }).collect()
 }
 
-fn repeating_pattern(n: usize) -> impl Iterator<Item=i32> {
-    iter::repeat(0).take(n+1)
-        .chain(iter::repeat(1).take(n+1))
-        .chain(iter::repeat(0).take(n+1))
-        .chain(iter::repeat(-1).take(n+1))
-        .cycle()
-        .skip(1)
+/// Hand back windows of values and n's that they need multiplying with.
+/// Ignore ranges that need multiplying with 0 since they don't contribute.
+/// Added complexity as we have to skip the first of the repeating pattern.
+fn repeating_indexes(n: usize, input: &[i8]) -> impl Iterator<Item=(&[i8],i32)> {
+    let first_chunk = iter::once(&input[0..n]).zip(iter::repeat(&0));
+    let next_chunks = input[n..].chunks(n+1).zip([1,0,-1,0].into_iter().cycle());
+    first_chunk.chain(next_chunks).filter_map(move |(chunk,&n)| {
+        if n == 0 {
+            None
+        } else {
+            Some((chunk, n))
+        }
+    })
 }
 
-fn parse_input(input: &str) -> Vec<i32> {
-    input.trim().bytes().map(|b| (b - 48) as i32).collect()
+fn parse_input(input: &str) -> Vec<i8> {
+    input.trim().bytes().map(|b| (b - 48) as i8).collect()
 }
